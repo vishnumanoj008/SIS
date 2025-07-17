ba7fa085eab97e4cdc136c33a8119d76e1f9a7c6
vishnumanoj008 <teamvctor@gmail.com>
2025-07-17 15:01:00 +0530

    updated localhost to render url and updated cors

--- backend/app.py
+++ backend/app.py
@@ -21,7 +21,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://vishnumanoj008.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
